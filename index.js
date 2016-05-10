36e0db24673cf8cbd6f7ac77cdab92bde529d522
danielliu1973 <daniel.liu1973@gmail.com>
2016-05-10 13:41:16 -0400

    k

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-//http://teropa.info/blog/2013/11/03/make-your-own-angular-part-1-scopes-and-digest.html
+//http://www.html5rocks.com/en/tutorials/es6/promises/
+//https://www.promisejs.org/implementing/
 
-function Scope() {
+var PENDING = 0;
+var FULFILLED = 1;
+var REJECTED = 2;
+
+function Promise() {
+  var state = PENDING;
+  var value = null;
+  var handlers = [];
 }
