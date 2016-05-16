2fc8e7488622cbb94441e49139436dcd24cd7e9a
danielliu1973 <daniel.liu1973@gmail.com>
2016-05-16 18:02:48 -0400

    watch/digest

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 //http://www.html5rocks.com/en/tutorials/es6/promises/
 //https://www.promisejs.org/implementing/
 
+new Promise(function(resolve, reject){
+	console.log('Promise');
+	resolve();
+	reject();
+}).then(
+	function(e){
+		console.log('then resolve');
+	},
+	function(e){
+		console.log('then reject');
+	}
+);
 var PENDING = 0;
-var FULFILLED = 1;
+var RESOLVED = 1;
 var REJECTED = 2;
 
-function Promise() {
+function Promise(func) {
   var state = PENDING;
   var value = null;
   var handlers = [];
+  var _resolve = {};
+  var _reject = {}
+  this.then = function(resolve, reject){
+  	_resolve = resolve;
+  	_reject = reject;
+  };
+  // func.call(this, _resolve, _reject);
+  // _resolve();
 }
